8a3ffe42761d3bbcdd62207782e247fc51f4325a
pudyasto <mr.pudyasto@gmail.com>
2020-07-17 13:52:06 +0700

    update title home

--- app/Views/template-frontend/main.php
+++ app/Views/template-frontend/main.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Pudyasto Adi - Portofolio</title>
+    <title>Portofolio</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
