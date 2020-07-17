5c875d0500943209da5a38f7602205f525f9b3c6
pudyasto <mr.pudyasto@gmail.com>
2020-07-17 11:02:30 +0700

    Update autload.php

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -55,7 +55,7 @@ class Autoload extends \CodeIgniter\Config\AutoloadConfig
 			'App'         => APPPATH,                // To ensure filters, etc still found,
 			APP_NAMESPACE => APPPATH,                // For custom namespace
 			'Config'      => APPPATH . 'Config',
-			'Myth\Auth'   => APPPATH . 'ThirdParty/myth-auth/src',
+			'Myth\Auth'   => APPPATH . 'ThirdParty/myth-auth-dev/src',
 		];
 
 		/**
